17fe015a80a6b83e5e4f50ce0c4b4de2c9663d41
alnahian2003 <a.alnahian2003@gmail.com>
2022-06-21 20:13:46 +0600

    Basic Configuration Completed

--- index.php
+++ index.php
@@ -9,3 +9,31 @@
 
  * Author Portfolio: https://alnahian2003.github.io
  */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- INTER Google Font CDN -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <title>Splinter — TODO List Web App</title>
+</head>
+
+<body>
+    <!-- Main JavaScript -->
+    <script src="assets/js/app.js"></script>
+</body>
+
+</html>
