81e6dd2759878399dd717c22cadf7a5f6a8ba6e5
alnahian2003 <a.alnahian2003@gmail.com>
2022-06-21 19:51:03 +0600

    project initialized

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Project Name: Splinter
+ * Project Type: TODO List Web App
+ * Project Author: Al Nahian
+ * Date Created: 7:48 PM, 21 Jun 2022
+ * GitHub: https://github.com/alnahian2003/splinter
+
+ * Author Portfolio: https://alnahian2003.github.io
+ */
