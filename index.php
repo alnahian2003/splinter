26f4ac6fcd1ed48013cb7f3e50cc8498afe9bbce
alnahian2003 <a.alnahian2003@gmail.com>
2022-06-22 13:21:17 +0600

    basic html markup completed

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
  * Author Portfolio: https://alnahian2003.github.io
  */
 
+// Set Default Time Zone
+date_default_timezone_set("Asia/Dhaka");
+
+// Require Functions.php
+// require_once("functions.php");
+
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +38,225 @@
 </head>
 
 <body>
+    <!-- Header Section: Intro Part, TODO Form Inputs -->
+    <header>
+        <div id="intro">
+            <h1>SPLINTER</h1>
+            <h3>TODO List Web App</h3>
+        </div>
+
+        <div id="todoForm">
+            <form action="index.php" method="POST">
+                <input type="text" name="title" id="todo_title" placeholder="start writing your todo task">
+
+                <input type="text" name="details" id="todo_details" placeholder="don't miss out to write the details, please!">
+                <button type="submit">+</button>
+            </form>
+        </div>
+    </header>
+
+
+    <!-- Main Section: TODO Cards -->
+    <main>
+        <h2>TASKS</h2>
+
+        <!-- Todo Cards Grid -->
+        <div id="todo">
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+
+            <!-- Todo Card -->
+            <div class="todo_card">
+                <!-- Card Top: Todo Number, Action Buttons -->
+                <div class="card_top">
+                    <p class="card_number">1</p>
+                    <div class="action_buttons">
+                        <button class="btn btn_finished">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        </button>
+                        <button class="btn btn_trash">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Card Center: Todo Title, Todo Details -->
+                <div class="card_center">
+                    <h4>Generate Sitemap in WordPress...</h4>
+                    <p>Generate sitemap in a wordpress website as soon as possible, hack the google 😅</p>
+                </div>
+
+                <!-- Card Bottom: Todo Date -->
+                <div class="card_bottom">
+                    <p><?= date("h:i, d M, Y", time()); ?></p>
+                </div>
+            </div>
+        </div>
+    </main>
+
     <!-- Main JavaScript -->
     <script src="assets/js/app.js"></script>
 </body>
